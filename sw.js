efd22472ea9f30e80d4e2ac4cc9f98e114f4c56e
Madalina Buzatu <madalina.buzatu@generali.com>
2021-10-28 12:12:56 +0200

    1

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@ self.addEventListener('activate', (event) => {
   // console.log('Inside the fetch handler:', event);
 // });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     //fetchWithParamAddedToRequestBody(event.request)
 	console.log('event.respondWith');
@@ -38,40 +38,40 @@ self.addEventListener('fetch', function(event) {
   );
 });
 
-function fetchWithParamAddedToRequestBody(request) {
-  serialize(request).then(function(serialized) {
-    // modify serialized.body here to add your request parameter
-    deserialize(serialized).then(function(request) {
-      return fetch(request);
-    });
-  }); // fixed this
-}
+// function fetchWithParamAddedToRequestBody(request) {
+  // serialize(request).then(function(serialized) {
+    // // modify serialized.body here to add your request parameter
+    // deserialize(serialized).then(function(request) {
+      // return fetch(request);
+    // });
+  // }); // fixed this
+// }
 
-function serialize(request) {
-  var headers = {};
-  for (var entry of request.headers.entries()) {
-    headers[entry[0]] = entry[1];
-  }
-  var serialized = {
-    url: request.url,
-    headers: headers,
-    method: request.method,
-    mode: request.mode,
-    credentials: request.credentials,
-    cache: request.cache,
-    redirect: request.redirect,
-    referrer: request.referrer
-  };  
-   console.log('SERIALIZED');
-  console.log(serialized);
-  if (request.method !== 'GET' && request.method !== 'HEAD') {
-    return request.clone().text().then(function(body) {
-      serialized.body = body;
-      return Promise.resolve(serialized);
-    });
-  }
-  return Promise.resolve(serialized);
-}
-function deserialize(data) {
-  return Promise.resolve(new Request(data.url, data));
-}
+// function serialize(request) {
+  // var headers = {};
+  // for (var entry of request.headers.entries()) {
+    // headers[entry[0]] = entry[1];
+  // }
+  // var serialized = {
+    // url: request.url,
+    // headers: headers,
+    // method: request.method,
+    // mode: request.mode,
+    // credentials: request.credentials,
+    // cache: request.cache,
+    // redirect: request.redirect,
+    // referrer: request.referrer
+  // };  
+   // console.log('SERIALIZED');
+  // console.log(serialized);
+  // if (request.method !== 'GET' && request.method !== 'HEAD') {
+    // return request.clone().text().then(function(body) {
+      // serialized.body = body;
+      // return Promise.resolve(serialized);
+    // });
+  // }
+  // return Promise.resolve(serialized);
+// }
+// function deserialize(data) {
+  // return Promise.resolve(new Request(data.url, data));
+// }
